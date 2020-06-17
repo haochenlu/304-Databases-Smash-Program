600a4782aad4a721c903d13cbee35553520e042c
Hao Chen Lu <haochen.lu@gmail.com>
2020-06-17 01:39:55 -0700

    prior to beginning menu folders

--- src/gui/gui/QueryEvent.java
+++ src/gui/gui/QueryEvent.java
@@ -3,9 +3,9 @@ package gui;
 import java.util.EventObject;
 
 public class QueryEvent extends EventObject {
-    private String[] params;
+    private int params;
     private String[] characters;
-    public QueryEvent(Object source, String[] params, String[] characters) {
+    public QueryEvent(Object source, int params, String[] characters) {
         super(source);
         this.params = params;
         this.characters = characters;
