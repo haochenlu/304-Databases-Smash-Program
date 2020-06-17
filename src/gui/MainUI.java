9b86bcac60275f199402f1a4ea7eee1c1a6fbb71
Hao Chen Lu <haochen.lu@gmail.com>
2020-06-16 23:59:03 -0700

    text in boxes

--- src/gui/MainUI.java
+++ src/gui/MainUI.java
@@ -3,6 +3,7 @@ package gui;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class MainUI {
     private JPanel panel1;
@@ -15,10 +16,56 @@ public class MainUI {
     protected enum characterList {
         DR_MARIO,
         MARIO,
-
+        LUIGI,
+        BOWSER,
+        PEACH,
+        YOSHI,
+        DONKEY_KONG,
+        CAPTAIN_FALCON,
+        GANONDORF,
+        FALCO,
+        FOX,
+        NESS,
+        ICE_CLIMBERS,
+        KIRBY,
+        SAMUS,
+        ZELDA,
+        LINK,
+        YOUNG_LINK,
+        PICHU,
+        PIKACHU,
+        JIGGLYPUFF,
+        MEWTWO,
+        GAME_AND_WATCH,
+        MARTH,
+        ROY
     }
 
+    protected enum stageList {
+        YOSHIS_STORY,
+        FINAL_DESTINATION,
+        FOUNTAIN_OF_DREAMS,
+        DREAMLAND,
+        BATTLEFIELD,
+        POKEMON_STADIUM
+    }
+
+
+    public static String[] getNames(Class<? extends Enum<?>> e) {
+        return Arrays.stream(e.getEnumConstants()).map(Enum::name).toArray(String[]::new);
+    }
+
+
     public MainUI() {
+        String[] character_array = getNames(characterList.class);
+        String[] stage_array = getNames(stageList.class);
+        for (String character : character_array) {
+            character_box_1.addItem(character);
+            character_box_2.addItem(character);
+        }
+        for (String stage : stage_array) {
+            stage_box.addItem(stage);
+        }
         character_box_1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -28,6 +75,7 @@ public class MainUI {
         character_box_2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
 
             }
         });
