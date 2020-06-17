dfce5ed935f28b901b8cdadc0bbcb01b729a2905
Matej Kalina <l4c2b@ugrad.cs.ubc.ca>
2020-06-16 22:30:03 -0700

    Added basic connection stuff

---
+++ src/main/Launcher.java
@@ -0,0 +1,9 @@
+public class Launcher {
+
+    public static void main(String[] args) {
+        Backend backend = new Backend();
+        backend.login("ora_mkalina", "a92772482");
+        backend.query("");
+        backend.close();
+    }
+}
