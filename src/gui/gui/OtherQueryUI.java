fedf75b75a6617c9b102767a6ac181ed2295b14e
Hao Chen Lu <haochen.lu@gmail.com>
2020-06-17 19:48:35 -0700

    second card window

--- src/gui/gui/OtherQueryUI.java
+++ src/gui/gui/OtherQueryUI.java
@@ -12,4 +12,25 @@ public class OtherQueryUI {
     private JTextField textField3;
     private JButton insertButton;
     private JButton deleteButton;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton updateButton;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JButton selectButton;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton joinButton;
+    private JButton aggregateButton;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JComboBox comboBox5;
+    private JComboBox comboBox6;
+    private JButton nestedAggregationButton;
+    private JButton divisionButton;
+    private JComboBox comboBox7;
 }
