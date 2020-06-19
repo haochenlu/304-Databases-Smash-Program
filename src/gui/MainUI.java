fe72d71d4440193591354e635368f5105b8681a4
Hao Chen Lu <haochen.lu@gmail.com>
2020-06-18 21:00:47 -0700

    descriptions

--- src/gui/MainUI.java
+++ src/gui/MainUI.java
@@ -142,7 +142,9 @@ public class MainUI implements ActionListener {
                                 "from characterdata c1, characterdata c2, fights f, stages st\n" +
                                 "where c1.cID = '" + char1 + "' AND c2.cID = '" + char2 + "' AND f.c1ID = c1.cID AND f.c2ID = c2.cID AND f.stID = st.stID AND st.stID = '" + stage + "'\n", 8);
                 System.out.println(result);
-                ResultPopup resultPopup = new ResultPopup(result);
+                ResultPopup resultPopup = new ResultPopup(result, "select c1.cID, c1.weight, c1.gravity, c2.cID, c2.weight, c2.gravity, st.stID, st.s\n" +
+                        "from characterdata c1, characterdata c2, fights f, stages st\n" +
+                        "where c1.cID = '" + char1 + "' AND c2.cID = '" + char2 + "' AND f.c1ID = c1.cID AND f.c2ID = c2.cID AND f.stID = st.stID AND st.stID = '" + stage + "'\n");
                 createPopup(resultPopup);
             }
         });
